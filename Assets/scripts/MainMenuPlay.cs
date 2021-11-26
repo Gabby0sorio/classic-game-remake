fbb142cee581af0b136baa03ba0b85cdf5c8936a
Elijah Bove <ebove@ltu.edu>
2021-11-25 20:47:24 -0500

    Main Menu
    .

---
+++ Assets/scripts/MainMenuPlay.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuPlay : MonoBehaviour {
+
+    public void SampleScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
